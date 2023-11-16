4c6ffff48e7f1273bd6afdeba139ccb587a2f345
Devangana26 <devanganabarua@gmail.com>
2023-11-16 13:13:13 +0530

    fixed carousal image rendering issue

--- src/App.js
+++ src/App.js
@@ -12,8 +12,9 @@ import {
 
 import Login from './screens/Login';
 import  '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
-import '../node_modules/bootstrap-dark-5/dist/js/darkmode.js';
-import '../node_modules/bootstrap-dark-5/dist/js/darkmode.min.js';
+// import '../node_modules/bootstrap-dark-5/dist/js/darkmode.js';
+// import '../node_modules/bootstrap-dark-5/dist/js/darkmode.min.js';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
   return (
